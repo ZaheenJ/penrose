98260b762e9f5e867039a77dc1114b6a97ab9fbb
ZaheenJ <zaheen.jamil@gmail.com>
2022-06-24 17:10:56 -0700

    smart borders and gaps still apply as long as only one window is tiled, floating windows aren't affected by smart borders

--- src/core/manager/layout.rs
+++ src/core/manager/layout.rs
@@ -3,7 +3,7 @@ use crate::{
         config::Config,
         hooks::HookName,
         manager::{event::EventAction, state::WmState, util::pad_region},
-        xconnection::{XClientConfig, XClientHandler},
+        xconnection::{XClientConfig, XClientHandler, Xid},
     },
     Result,
 };
@@ -41,6 +41,7 @@ where
         mut gap_px,
         ..
     } = state.config;
+    let float_border_px = border_px;
 
     let (lc, aa) = state.workspaces.get_arrange_actions(
         wix,
@@ -50,7 +51,7 @@ where
             .clients_for_ids(&state.workspaces[wix].client_ids()),
     )?;
 
-    if state.workspaces.get_workspace(wix)?.len() == 1 {
+    if state.workspaces.get_workspace(wix)?.len() <= aa.floating.len() + 1 {
         if lc.smart_borders {
             border_px = 0;
         }
@@ -62,6 +63,10 @@ where
     for (id, region) in aa.actions {
         trace!(id, ?region, "positioning client");
         if let Some(region) = region {
+            let mut border_px = border_px;
+            if aa.floating.contains(&id) {
+                border_px = float_border_px;
+            };
             let reg = pad_region(&region, lc.gapless, gap_px, border_px);
             conn.position_client(id, reg, border_px, false)?;
             state.clients.map_if_needed(id, conn)?;
